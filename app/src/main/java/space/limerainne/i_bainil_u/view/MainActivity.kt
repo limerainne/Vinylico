d110023812c62bc6673dc8aae24ce56fdad73429
Jongsung Kang <jskang@cottoncandy>
2016-10-17 07:18:54 +0900

    add searchview

--- app/src/main/java/space/limerainne/i_bainil_u/view/MainActivity.kt
+++ app/src/main/java/space/limerainne/i_bainil_u/view/MainActivity.kt
@@ -145,6 +145,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
+
+
+
         return true
     }
 
