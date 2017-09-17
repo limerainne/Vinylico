f56c176b0a14303f52e82afca8c3adc67f6f07fc
Limerainne <limerainne@gmail.com>
2017-09-17 12:14:49 +0900

    add response example in artist request

--- app/src/main/java/win/limerainne/vinylico/data/api/request/data/RequestArtistAlbumList.kt
+++ app/src/main/java/win/limerainne/vinylico/data/api/request/data/RequestArtistAlbumList.kt
@@ -12,6 +12,88 @@ import win.limerainne.vinylico.data.api.request.Request
 /**
  * Created by Limerainne on 2016-07-21.
  */
+/* @17.09.17
+ - http://www.bainil.com/api/v2/artist/albums?artistId=1005&lang=ko
+
+{
+  "result": [
+    {
+      "albumEnglish": "Lovelyz 2nd Album Repackage 'Now We'",
+      "albumName": "Lovelyz 2nd Album Repackage [지금, 우리]",
+      "albumId": 4195,
+      "albumType": 0,
+      "artistEnglish": "Lovelyz",
+      "feature_rec": false,
+      "feature_hd": false,
+      "tracks": 13,
+      "backImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/4195/34c14851-054c-4561-bc73-a93c5c12873b_640.jpg",
+      "feature_lyrics": true,
+      "releaseDate": "2017-05-02",
+      "price": 8.99,
+      "artistId": 1005,
+      "owner": 1,
+      "feature_aac": false,
+      "feature_booklet": false,
+      "cdImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/4195/891e8986-fd0d-46a2-8ac5-f490dfbec868_570.jpg",
+      "artistName": "러블리즈",
+      "genreName": "Dance-pop",
+      "feature_adult": false,
+      "jacketImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/4195/0bb66b1a-f31c-4d93-b510-e940d43b4154_360.jpg",
+      "genreId": 1
+    },
+    {
+      "albumEnglish": "Lovelyz 2nd Album `R U Ready?`",
+      "albumName": "Lovelyz 2nd Album `R U Ready?`",
+      "albumId": 3872,
+      "albumType": 0,
+      "artistEnglish": "Lovelyz",
+      "feature_rec": false,
+      "feature_hd": false,
+      "tracks": 11,
+      "backImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/3872/b1f80974-2bbe-47ce-999f-81d865366d29_640.jpg",
+      "feature_lyrics": true,
+      "releaseDate": "2017-02-26",
+      "price": 8.99,
+      "artistId": 1005,
+      "owner": 1,
+      "feature_aac": false,
+      "feature_booklet": false,
+      "cdImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/3872/37a5d51d-2893-4357-8691-3e83202d335d_570.jpg",
+      "artistName": "러블리즈",
+      "genreName": "Dance-pop",
+      "feature_adult": false,
+      "jacketImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/3872/cc6e14f1-352b-4432-b648-7bd41d84bbe5_360.jpg",
+      "genreId": 1
+    },
+    {
+      "albumEnglish": "A New Trilogy",
+      "albumName": "A New Trilogy",
+      "albumId": 2423,
+      "albumType": 0,
+      "artistEnglish": "Lovelyz",
+      "feature_rec": false,
+      "feature_hd": false,
+      "tracks": 7,
+      "backImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/2423/d4f88082-3c72-4a12-a158-7d82cdb64f73_640.jpg",
+      "feature_lyrics": true,
+      "releaseDate": "2016-04-25",
+      "price": 5.99,
+      "artistId": 1005,
+      "owner": 1,
+      "feature_aac": false,
+      "feature_booklet": true,
+      "cdImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/2423/e523429e-9301-436b-8994-bd8a739f6647_570.jpg",
+      "artistName": "러블리즈",
+      "genreName": "Teen Pop",
+      "feature_adult": false,
+      "jacketImage": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/album/2423/92b73ea3-a5c5-415e-a71b-fc05561d806d_360.jpg",
+      "genreId": 3
+    }
+  ],
+  "success": true
+}
+
+ */
 class RequestArtistAlbumList(val artistId: Long,
                              val lang: String = ThisApp.LangCode,
                              val gson: Gson = Gson()) : RequestHTTPConnection() {
--- app/src/main/java/win/limerainne/vinylico/data/api/request/data/RequestArtistDetail.kt
+++ app/src/main/java/win/limerainne/vinylico/data/api/request/data/RequestArtistDetail.kt
@@ -11,6 +11,30 @@ import win.limerainne.vinylico.data.api.request.Request
 /**
  * Created by Limerainne on 2016-07-21.
  */
+/* @17.09.17
+ - http://www.bainil.com/api/v2/artist?artistId=1005&lang=ko
+
+{
+  "result": {
+    "countryName": "South Korea",
+    "fans": 26,
+    "artistEnglish": "Lovelyz",
+    "countryNo": 112,
+    "labelId": 5043,
+    "homepage": "http://www.lvlz8.com/main_teaser_bi11_0831.html#5",
+    "twitter": "https://twitter.com/Official_LVLZ",
+    "artistPicture": "https://d3ttbrlvlwtn7f.cloudfront.net/upload/artist/1005/6851383d-45d7-4771-b300-18d4d5e9bc30_360.jpg",
+    "artistId": 1005,
+    "facebook": "https://www.facebook.com/lvlz8",
+    "artistDescEnglish": "Eight-member girl group Lovelyz made their debut in November 2014 with their official album <Girls’ Invasion>. Like the name of the group, Lovelyz have a  lovely appearance and music, which successfully drew many people’s attention. They have performed actively and proved to be a No.1 girl group of the next generation by being voted as the best new female idol.   ",
+    "youtube": "https://www.youtube.com/user/lvlz8",
+    "labelName": "울림 엔터테인먼트",
+    "artistDesc": "러블리즈 는 8인조 걸그룹으로 2014년 11월 정규앨범《 Girls' Invasion 》으로 데뷔했다. 러블리즈는 2015년 최고의 신인 여자 아이돌 설문조사에서 1위를 차지하며 차세대 청순 걸그룹 대표주자임을 증명하며, 활발할 활동을 펼치고 있다.",
+    "artistName": "러블리즈"
+  },
+  "success": true
+}
+ */
 class RequestArtistDetail(val artistId: Long,
                           val lang: String = ThisApp.LangCode,
                           val gson: Gson = Gson()) : RequestHTTPConnection() {
